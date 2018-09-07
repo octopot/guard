fc937c3e2cc1cedb31f0ecd0cefaa2756c9bb805
Kamil Samigullin <kamil@samigullin.info>
2018-09-07 17:20:54 +0300

    review examples

--- pkg/transport/http/router/chi/router.go
+++ pkg/transport/http/router/chi/router.go
@@ -15,7 +15,7 @@ func Configure(api router.API) http.Handler {
 	r.Use(middleware.RealIP)
 	r.Use(middleware.Logger)
 	r.Route("/api/v1/license", func(r chi.Router) {
-		r.Get("/check", api.CheckLicenseV1)
+		r.Post("/check", api.CheckLicenseV1)
 		r.Put("/extend", api.ExtendLicenseV1)
 		r.Post("/register", api.RegisterLicenseV1)
 	})
