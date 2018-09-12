7786ff6aff232217911ed411263eb8eaad9ec9b6
Kamil Samigullin <kamil@samigullin.info>
2018-09-12 16:37:19 +0300

    add auth middleware

---
+++ pkg/transport/grpc/middleware/auth.go
@@ -0,0 +1,39 @@
+package middleware
+
+import (
+	"context"
+
+	"github.com/grpc-ecosystem/go-grpc-middleware/auth"
+	"github.com/kamilsk/guard/pkg/service/types"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+const (
+	// AuthHeader defines authorization header.
+	AuthHeader = "authorization"
+	// AuthScheme defines authorization scheme.
+	AuthScheme = "bearer"
+)
+
+// TokenInjector TODO issue#docs
+func TokenInjector(ctx context.Context) (context.Context, error) {
+	token, err := grpc_auth.AuthFromMD(ctx, AuthScheme)
+	if err != nil {
+		return nil, err
+	}
+	tokenID := types.ID(token)
+	if !tokenID.IsValid() {
+		return nil, status.Errorf(codes.Unauthenticated, "invalid auth token: %s", token)
+	}
+	return context.WithValue(ctx, tokenKey{}, tokenID), nil
+}
+
+// TokenExtractor TODO issue#docs
+func TokenExtractor(ctx context.Context) (types.ID, error) {
+	tokenID, found := ctx.Value(tokenKey{}).(types.ID)
+	if !found {
+		return tokenID, status.Error(codes.Unauthenticated, "auth token not found")
+	}
+	return tokenID, nil
+}
---
+++ pkg/transport/http/middleware/auth.go
@@ -0,0 +1,47 @@
+package middleware
+
+import (
+	"context"
+	"net/http"
+	"strings"
+
+	"github.com/kamilsk/guard/pkg/service/types"
+	"github.com/pkg/errors"
+)
+
+const (
+	// AuthHeader defines authorization header.
+	AuthHeader = "authorization"
+	// AuthScheme defines authorization scheme.
+	AuthScheme = "bearer"
+)
+
+// TokenInjector TODO issue#docs
+func TokenInjector(req *http.Request) (*http.Request, error) {
+	header := req.Header.Get(AuthHeader)
+	if header == "" {
+		return nil, errors.New("auth token not found")
+	}
+	splits := strings.SplitN(header, " ", 2)
+	if len(splits) < 2 {
+		return nil, errors.New("bad authorization string")
+	}
+	scheme, token := splits[0], splits[1]
+	if !strings.EqualFold(scheme, AuthScheme) {
+		return nil, errors.Errorf("request unauthenticated with %s", AuthScheme)
+	}
+	tokenID := types.ID(token)
+	if !tokenID.IsValid() {
+		return nil, errors.Errorf("invalid auth token: %s", token)
+	}
+	return req.WithContext(context.WithValue(req.Context(), tokenKey{}, tokenID)), nil
+}
+
+// TokenExtractor TODO issue#docs
+func TokenExtractor(req *http.Request) (types.ID, error) {
+	tokenID, found := req.Context().Value(tokenKey{}).(types.ID)
+	if !found {
+		return tokenID, errors.New("auth token not found")
+	}
+	return tokenID, nil
+}
