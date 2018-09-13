0413c0067216b4f15c9c5b028a0d7c6974dd972e
Kamil Samigullin <kamil@samigullin.info>
2018-09-13 11:44:41 +0300

    fix #4: define grpc boilerplate

--- cmd/ctl.go
+++ cmd/ctl.go
@@ -1,6 +1,57 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"fmt"
+	"strconv"
+	"time"
 
-// Control communicates with Guard server via gRPC.
-var Control = &cobra.Command{}
+	"github.com/kamilsk/go-kit/pkg/fn"
+	"github.com/kamilsk/go-kit/pkg/strings"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+const (
+	yamlFormat = "yaml"
+	jsonFormat = "json"
+)
+
+var (
+	// License TODO issue#docs
+	License         = &cobra.Command{Use: "license", Short: "Guard License"}
+	registerLicense = &cobra.Command{Use: "register", Short: "Register user license", RunE: communicate}
+	extendLicense   = &cobra.Command{Use: "extend", Short: "Extend user license", RunE: communicate}
+	checkLicense    = &cobra.Command{Use: "check", Short: "Check user license", RunE: communicate}
+)
+
+func init() {
+	v := viper.New()
+	fn.Must(
+		func() error { return v.BindEnv("bind") },
+		func() error { return v.BindEnv("grpc_port") },
+		func() error { return v.BindEnv("guard_token") },
+		func() error {
+			v.SetDefault("bind", defaults["bind"])
+			v.SetDefault("grpc_port", defaults["grpc_port"])
+			v.SetDefault("grpc_host", strings.Concat(v.GetString("bind"), ":", strconv.Itoa(v.GetInt("grpc_port"))))
+			v.SetDefault("guard_token", defaults["guard_token"])
+			return nil
+		},
+		func() error {
+			flags := License.PersistentFlags()
+			flags.StringVarP(new(string), "filename", "f", "", "entity source (default is standard input)")
+			flags.StringVarP(new(string), "output", "o", yamlFormat, fmt.Sprintf(
+				"output format, one of: %s|%s",
+				jsonFormat, yamlFormat))
+			flags.Bool("dry-run", false, "if true, only print the object that would be sent, without sending it")
+			flags.StringVarP(&cnf.Union.GRPCConfig.Interface,
+				"grpc-host", "", v.GetString("grpc_host"), "gRPC server host")
+			flags.DurationVarP(&cnf.Union.GRPCConfig.Timeout,
+				"timeout", "t", time.Second, "connection timeout")
+			flags.StringVarP((*string)(&cnf.Union.GRPCConfig.Token),
+				"token", "", v.GetString("guard_token"), "user access token")
+			return nil
+		},
+	)
+	License.AddCommand(registerLicense, extendLicense, checkLicense)
+}
---
+++ cmd/grpc_helper.go
@@ -0,0 +1,128 @@
+package cmd
+
+import (
+	"context"
+	"encoding/json"
+	"io"
+	"io/ioutil"
+	"os"
+
+	pb "github.com/kamilsk/guard/pkg/transport/grpc"
+
+	"github.com/kamilsk/go-kit/pkg/strings"
+	"github.com/kamilsk/guard/pkg/config"
+	"github.com/kamilsk/guard/pkg/transport/grpc/middleware"
+	"github.com/mitchellh/mapstructure"
+	"github.com/pkg/errors"
+	"github.com/spf13/cobra"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/metadata"
+	"gopkg.in/yaml.v2"
+)
+
+var entities factory
+
+func init() {
+	entities = factory{
+		checkLicense:    {"License": func() pb.Proxy { return pb.CheckLicenseRequestProxy{} }},
+		extendLicense:   {"License": func() pb.Proxy { return pb.ExtendLicenseRequestProxy{} }},
+		registerLicense: {"License": func() pb.Proxy { return pb.RegisterLicenseRequestProxy{} }},
+	}
+}
+
+func communicate(cmd *cobra.Command, _ []string) error {
+	entity, err := entities.proxy(cmd)
+	if err != nil {
+		return err
+	}
+	if dry, _ := cmd.Flags().GetBool("dry-run"); dry {
+		cmd.Printf("%T would be sent with data: ", entity)
+		if cmd.Flag("output").Value.String() == jsonFormat {
+			return json.NewEncoder(cmd.OutOrStdout()).Encode(entity)
+		}
+		return yaml.NewEncoder(cmd.OutOrStdout()).Encode(entity)
+	}
+	response, err := call(cnf.Union.GRPCConfig, entity)
+	if err != nil {
+		return err
+	}
+	if cmd.Flag("output").Value.String() == jsonFormat {
+		return json.NewEncoder(cmd.OutOrStdout()).Encode(response)
+	}
+	return yaml.NewEncoder(cmd.OutOrStdout()).Encode(response)
+}
+
+type builder func() pb.Proxy
+type kind string
+type field string
+type value interface{}
+
+type schema struct {
+	Kind    kind            `yaml:"kind"`
+	Payload map[field]value `yaml:"payload"`
+}
+
+type factory map[*cobra.Command]map[kind]builder
+
+func (factory) scheme(filename string) (schema, error) {
+	var (
+		err error
+		out schema
+		raw []byte
+		src io.Reader = os.Stdin
+	)
+	if filename != "" {
+		if src, err = os.Open(filename); err != nil {
+			return out, errors.Wrapf(err, "trying to open file %q", filename)
+		}
+	} else {
+		filename = "/dev/stdin"
+	}
+	if raw, err = ioutil.ReadAll(src); err != nil {
+		return out, errors.Wrapf(err, "trying to read file %q", filename)
+	}
+	err = yaml.Unmarshal(raw, &out)
+	return out, errors.Wrapf(err, "trying to decode file %q as YAML", filename)
+}
+
+func (f factory) proxy(cmd *cobra.Command) (pb.Proxy, error) {
+	scheme, err := f.scheme(cmd.Flag("filename").Value.String())
+	if err != nil {
+		return nil, err
+	}
+	build, ok := f[cmd][scheme.Kind]
+	if !ok {
+		return nil, errors.Errorf("unknown payload type %q", scheme.Kind)
+	}
+	entity := build()
+	if err = mapstructure.Decode(scheme.Payload, &entity); err != nil {
+		return nil, errors.Wrapf(err, "trying to decode payload to %#v", entity)
+	}
+	return entity, nil
+}
+
+func call(cnf config.GRPCConfig, entity pb.Proxy) (interface{}, error) {
+	deadline, cancel := context.WithTimeout(context.Background(), cnf.Timeout)
+	conn, err := grpc.DialContext(deadline, cnf.Interface, grpc.WithInsecure())
+	cancel()
+	if err != nil {
+		return nil, errors.Wrapf(err, "trying to connect to the gRPC server at %q", cnf.Interface)
+	}
+	defer conn.Close()
+	client := pb.NewLicenseClient(conn)
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	ctx = metadata.AppendToOutgoingContext(ctx,
+		middleware.AuthHeader,
+		strings.Concat(middleware.AuthScheme, " ", string(cnf.Token)))
+	switch request := entity.Convert().(type) {
+	case *pb.CheckLicenseRequest:
+		return client.Check(ctx, request)
+	case *pb.ExtendLicenseRequest:
+		return client.Extend(ctx, request)
+	case *pb.RegisterLicenseRequest:
+		return client.Register(ctx, request)
+	default:
+		return nil, errors.Errorf("unknown request type %T", request)
+	}
+}
