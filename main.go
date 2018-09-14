2833d9a492de8b35e0792df1fe82341199bc1de4
Kamil Samigullin <kamil@samigullin.info>
2018-09-14 22:45:21 +0300

    move import of unused packages to Gopkg.toml

--- main.go
+++ main.go
@@ -8,21 +8,6 @@ import (
 
 	"github.com/kamilsk/guard/cmd"
 	"github.com/spf13/cobra"
-
-	_ "github.com/lib/pq"
-	_ "github.com/mailru/easyjson"
-	_ "go.uber.org/zap"
-
-	_ "go.etcd.io/etcd/clientv3"
-	_ "go.etcd.io/etcd/clientv3/clientv3util"
-	_ "go.etcd.io/etcd/clientv3/concurrency"
-	_ "go.etcd.io/etcd/clientv3/integration"
-	_ "go.etcd.io/etcd/clientv3/leasing"
-	_ "go.etcd.io/etcd/clientv3/mirror"
-	_ "go.etcd.io/etcd/clientv3/namespace"
-	_ "go.etcd.io/etcd/clientv3/naming"
-	_ "go.etcd.io/etcd/clientv3/ordering"
-	_ "go.etcd.io/etcd/clientv3/yaml"
 )
 
 var service cli = func(executor commander, output io.Writer, shutdown func(code int)) {
