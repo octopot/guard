5bf85c13619b73a9aed6519dd0def539418a21e7
Kamil Samigullin <kamil@samigullin.info>
2018-09-17 20:25:39 +0300

    fix #18: add individual docker compose targets

--- env/make/common.mk
+++ env/make/common.mk
@@ -1,3 +1,3 @@
 .PHONY: help
-help: #| Shows available help information of Makefile.
+help:              #| Shows available help information of Makefile.
 	@fgrep -h "#|" $(MAKEFILE_LIST) | fgrep -v fgrep | sed -e 's/\\$$//' | sed -e 's/#|//'
--- env/make/docker-compose.mk
+++ env/make/docker-compose.mk
@@ -1,35 +1,86 @@
 COMPOSE ?= docker-compose -f env/docker/compose/docker-compose.base.yml -f env/docker/compose/docker-compose.dev.yml -p guard
 
 
-.PHONY: env
-env:
-	cp -nrf env/docker/compose/.env.example .env
-
-
+.PHONY: __env__
+__env__:
+	@(cp -nrf env/docker/compose/.env.example .env)
+#|
+#|                    ---
+#|
 .PHONY: config
-config: env
-	$(COMPOSE) config
-
+config: __env__    #| Validate and view the Compose file.
+	@($(COMPOSE) config)
+#|
 .PHONY: up
-up: env
-	$(COMPOSE) up -d
-
+up: __env__        #| Builds, (re)creates, starts, and attaches to containers for services.
+	@($(COMPOSE) up -d)
+#|
 .PHONY: fresh-up
-fresh-up: env
-	$(COMPOSE) up -d --build --force-recreate
-
-.PHONY: clear
-clear: env
-	$(COMPOSE) rm -f
-
+fresh-up: __env__  #| Builds images before starting containers,
+                   #| (re)creates containers even if their configuration and image haven't changed,
+                   #| starts, and attaches to containers for a service.
+	@($(COMPOSE) up -d --build --force-recreate)
+#|
+.PHONY: clean
+clean: __env__     #| Removes stopped service containers.
+	@($(COMPOSE) rm -f)
+#|
 .PHONY: down
-down: env
-	$(COMPOSE) down
-
+down: __env__      #| Stops containers and removes them with networks.
+	@($(COMPOSE) down)
+#|
 .PHONY: destroy
-destroy: env
-	$(COMPOSE) down --volumes --rmi local
-
+destroy: __env__   #| Stops containers and removes them with networks, volumes, and images created by `up`.
+	@($(COMPOSE) down --volumes --rmi local)
+#|
 .PHONY: status
-status: env
-	$(COMPOSE) ps
+status: __env__    #| List containers and their status.
+	@($(COMPOSE) ps)
+#|
+#|                    ---
+#|
+SERVICES = db \
+           legacy \
+           service \
+           server
+
+.PHONY: services
+services:          #| Shows available services.
+	@(echo 'available services:'; for container in $(SERVICES); do echo '-' $$container; done)
+
+define service_tpl
+#|
+.PHONY: up-$(1)
+up-$(1):           #| Builds, (re)creates, starts, and attaches to a container for the service $(1).
+                   #| For example `make up-server`. See `make services`.
+	@($$(COMPOSE) up -d $(1))
+#|
+.PHONY: container-$(1)
+container-$(1):    #| Enter to a running container of the service $(1).
+                   #| For example `make container-server`. See `make services`.
+	@($$(COMPOSE) exec $(1) sh)
+#|
+.PHONY: start-$(1)
+start-$(1):        #| Start an existing container of the service $(1).
+                   #| For example `make start-server`. See `make services`.
+	@($$(COMPOSE) start $(1))
+#|
+.PHONY: restart-$(1)
+restart-$(1):      #| Restart a running container of the service $(1).
+                   #| For example `make restart-server`. See `make services`.
+	@($$(COMPOSE) restart $(1))
+#|
+.PHONY: stop-$(1)
+stop-$(1):         #| Stop a running container of the service $(1) without removing them.
+                   #| For example `make stop-server`. See `make services`.
+	@($$(COMPOSE) stop $(1))
+#|
+.PHONY: log-$(1)
+log-$(1):          #| View output from a container of the service $(1).
+                   #| For example `make log-server`. See `make services`.
+	@($$(COMPOSE) logs -f $(1))
+#|
+endef
+
+render_service_tpl = $(eval $(call service_tpl,$(service)))
+$(foreach service,$(SERVICES),$(render_service_tpl))
