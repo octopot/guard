82768d7c9eb08d581c609be0a72f78f836d5a5c6
Kamil Samigullin <kamil@samigullin.info>
2018-09-23 18:18:58 +0300

    add license_workplace to the schema

--- pkg/storage/migrations/postgres/3_domain.sql
+++ pkg/storage/migrations/postgres/3_domain.sql
@@ -4,6 +4,7 @@ CREATE TABLE "license" (
   "number"       UUID      NOT NULL PRIMARY KEY DEFAULT uuid_generate_v4(),
   "active_since" TIMESTAMP NULL                 DEFAULT NULL,
   "active_until" TIMESTAMP NULL                 DEFAULT NULL,
+  -- {"rate_limits":"%d (rps|rpm|rph|rpw)", "request_limits": uint, "workplace_limits": uint}
   "contract"     JSONB     NULL                 DEFAULT NULL,
   "created_at"   TIMESTAMP NOT NULL             DEFAULT now(),
   "updated_at"   TIMESTAMP NULL                 DEFAULT NULL,
@@ -12,10 +13,10 @@ CREATE TABLE "license" (
 
 CREATE TABLE "license_audit" (
   "id"           BIGSERIAL PRIMARY KEY,
+  "number"       UUID      NOT NULL,
+  "what"         ACTION    NOT NULL,
   "when"         TIMESTAMP NOT NULL,
   "who"          UUID      NOT NULL,
-  "what"         ACTION    NOT NULL,
-  "number"       UUID      NOT NULL,
   -- before
   "active_since" TIMESTAMP NULL,
   "active_until" TIMESTAMP NULL,
@@ -27,6 +28,14 @@ CREATE TABLE "license_user" (
   "user_id"    UUID      NOT NULL,
   "created_at" TIMESTAMP NOT NULL             DEFAULT now(),
   UNIQUE ("license", "user_id")
+);
+
+CREATE TABLE "license_workplace" (
+  "license"    UUID      NOT NULL,
+  "workplace"  UUID      NOT NULL,
+  "created_at" TIMESTAMP NOT NULL             DEFAULT now(),
+  "updated_at" TIMESTAMP NULL                 DEFAULT NULL,
+  UNIQUE ("license", "workplace")
 );
 
 CREATE TRIGGER "license_updated"
@@ -48,6 +57,8 @@ ON "license_audit";
 
 DROP TRIGGER "license_updated"
 ON "license";
+
+DROP TABLE "license_workplace";
 
 DROP TABLE "license_user";
 
