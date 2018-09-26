a7f88adec6d43df42f785cc16988c5a8f698bd1a
Kamil Samigullin <kamil@samigullin.info>
2018-09-26 18:22:22 +0300

    issue #32: review domain part (license and license audit)

--- pkg/storage/migrations/postgres/3_domain.sql
+++ pkg/storage/migrations/postgres/3_domain.sql
@@ -2,8 +2,7 @@
 
 CREATE TABLE "license" (
   "number"     UUID      NOT NULL PRIMARY KEY DEFAULT uuid_generate_v4(),
-  -- {"since": ts, "until": ts, "rate_limits":"%d (rps|rpm|rph|rpw)", "request_limits": uint, "workplace_limits": uint}
-  "contract"   JSONB     NULL                 DEFAULT NULL,
+  "contract"   JSONB     NOT NULL,
   "created_at" TIMESTAMP NOT NULL             DEFAULT now(),
   "updated_at" TIMESTAMP NULL                 DEFAULT NULL,
   "deleted_at" TIMESTAMP NULL                 DEFAULT NULL
@@ -13,24 +12,10 @@ CREATE TABLE "license_audit" (
   "id"       BIGSERIAL PRIMARY KEY,
   "number"   UUID      NOT NULL,
   "what"     ACTION    NOT NULL,
-  "when"     TIMESTAMP NOT NULL,
   "who"      UUID      NOT NULL,
-  "contract" JSONB     NULL -- what was before
-);
-
-CREATE TABLE "license_user" (
-  "license"    UUID      NOT NULL,
-  "user_id"    UUID      NOT NULL,
-  "created_at" TIMESTAMP NOT NULL             DEFAULT now(),
-  UNIQUE ("license", "user_id")
-);
-
-CREATE TABLE "license_workplace" (
-  "license"    UUID      NOT NULL,
-  "workplace"  UUID      NOT NULL,
-  "created_at" TIMESTAMP NOT NULL             DEFAULT now(),
-  "updated_at" TIMESTAMP NULL                 DEFAULT NULL,
-  UNIQUE ("license", "workplace")
+  "when"     TIMESTAMP NOT NULL DEFAULT now(),
+  "with"     UUID      NOT NULL,
+  "contract" JSONB     NOT NULL
 );
 
 CREATE TRIGGER "license_updated"
@@ -47,15 +32,9 @@ CREATE TRIGGER "immutable_license_audit"
 
 -- +migrate Down
 
-DROP TRIGGER "immutable_license_audit"
-ON "license_audit";
+DROP TRIGGER "immutable_license_audit" ON "license_audit";
 
-DROP TRIGGER "license_updated"
-ON "license";
-
-DROP TABLE "license_workplace";
-
-DROP TABLE "license_user";
+DROP TRIGGER "license_updated" ON "license";
 
 DROP TABLE "license_audit";
 
