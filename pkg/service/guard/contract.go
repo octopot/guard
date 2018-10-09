708561152ae4fe98e7302642bf8c504a48f798d6
Kamil Samigullin <kamil@samigullin.info>
2018-10-09 17:50:55 +0300

    issue #30: extend storage api

--- pkg/service/guard/contract.go
+++ pkg/service/guard/contract.go
@@ -1,4 +1,15 @@
 package guard
 
+import (
+	"context"
+
+	repository "github.com/kamilsk/guard/pkg/storage/types"
+
+	"github.com/kamilsk/guard/pkg/storage/query"
+)
+
 // Storage TODO issue#docs
-type Storage interface{}
+type Storage interface {
+	// RegisterAccount TODO issue#docs
+	RegisterAccount(context.Context, *query.RegisterAccount) (*repository.Account, error)
+}
