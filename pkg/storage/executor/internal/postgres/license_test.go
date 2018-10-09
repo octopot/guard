d20a20c123705aa8aa17c9fd1c2dec487983a9a7
Kamil Samigullin <kamil@samigullin.info>
2018-10-09 12:33:38 +0300

    add sqlmock tests

--- pkg/storage/executor/internal/postgres/license_test.go
+++ pkg/storage/executor/internal/postgres/license_test.go
@@ -1,1 +1,40 @@
 package postgres_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/kamilsk/guard/pkg/storage/executor"
+
+	. "github.com/kamilsk/guard/pkg/storage/executor/internal/postgres"
+)
+
+func TestLicenseManager(t *testing.T) {
+	t.Run("create", func(t *testing.T) {
+		ctx, cancel := context.WithCancel(context.Background())
+		defer cancel()
+
+		var _ executor.LicenseManager = NewLicenseContext(ctx, nil)
+	})
+	t.Run("read", func(t *testing.T) {
+		ctx, cancel := context.WithCancel(context.Background())
+		defer cancel()
+
+		var _ executor.LicenseManager = NewLicenseContext(ctx, nil)
+	})
+	t.Run("update", func(t *testing.T) {
+		ctx, cancel := context.WithCancel(context.Background())
+		defer cancel()
+
+		var _ executor.LicenseManager = NewLicenseContext(ctx, nil)
+	})
+	t.Run("delete", func(t *testing.T) {
+		ctx, cancel := context.WithCancel(context.Background())
+		defer cancel()
+
+		var _ executor.LicenseManager = NewLicenseContext(ctx, nil)
+	})
+}
+
+func TestLicenseReader(t *testing.T) {
+}
