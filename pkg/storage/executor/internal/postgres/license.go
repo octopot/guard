c4853323c34cd98da7a0bd405d3bffca51de646b
Kamil Samigullin <kamil@samigullin.info>
2018-10-22 19:36:41 +0300

    issue #53: implement license reader

--- pkg/storage/executor/internal/postgres/license.go
+++ pkg/storage/executor/internal/postgres/license.go
@@ -81,6 +81,47 @@ func (scope licenseManager) Read(token *types.Token, data query.ReadLicense) (ty
 	return entity, nil
 }
 
+// ReadByID TODO issue#docs
+func (scope licenseManager) ReadByID(data query.ReadByIDLicense) (types.License, error) {
+	entity, encoded := types.License{ID: data.ID}, []byte(nil)
+	q := `SELECT "contract", "created_at", "updated_at"
+	        FROM "license"
+	       WHERE "id" = $1 AND "deleted_at" IS NULL`
+	row := scope.conn.QueryRowContext(scope.ctx, q, entity.ID)
+	if scanErr := row.Scan(&encoded, &entity.CreatedAt, &entity.UpdatedAt); scanErr != nil {
+		return entity, errors.Wrapf(scanErr, "trying to read license %q", entity.ID)
+	}
+	if decodeErr := json.Unmarshal(encoded, &entity.Contract); decodeErr != nil {
+		return entity, errors.Wrapf(decodeErr,
+			"trying to decode contract %s of license %q from JSON",
+			encoded, entity.ID)
+	}
+	return entity, nil
+}
+
+// ReadByEmployee TODO issue#docs
+func (scope licenseManager) ReadByEmployee(data query.ReadByEmployeeLicense) (types.License, error) {
+	entity, encoded := types.License{}, []byte(nil)
+	q := `SELECT "id", "contract", "created_at", "updated_at"
+	        FROM "license"
+	       WHERE "id" = (
+	             SELECT "license"
+	               FROM "license_employee"
+	              WHERE "employee" = $1
+	             )
+	         AND "deleted_at" IS NULL`
+	row := scope.conn.QueryRowContext(scope.ctx, q, data.Employee)
+	if scanErr := row.Scan(&entity.ID, &encoded, &entity.CreatedAt, &entity.UpdatedAt); scanErr != nil {
+		return entity, errors.Wrapf(scanErr, "trying to read license of employee %q", data.Employee)
+	}
+	if decodeErr := json.Unmarshal(encoded, &entity.Contract); decodeErr != nil {
+		return entity, errors.Wrapf(decodeErr,
+			"trying to decode contract %s of license %q from JSON",
+			encoded, entity.ID)
+	}
+	return entity, nil
+}
+
 // Update TODO issue#docs
 func (scope licenseManager) Update(token *types.Token, data query.UpdateLicense) (types.License, error) {
 	entity, readErr := scope.Read(token, query.ReadLicense{ID: data.ID})
@@ -200,3 +241,90 @@ func (scope licenseManager) Restore(token *types.Token, data query.RestoreLicens
 	entity.Account = token.User.Account
 	return entity, nil
 }
+
+// TODO issue#draft {
+
+// AddEmployee TODO issue#docs
+func (scope licenseManager) AddEmployee(token *types.Token, data query.LicenseEmployee) error {
+	license, readErr := scope.Read(token, query.ReadLicense{ID: data.ID})
+	if readErr != nil {
+		return readErr
+	}
+	q := `INSERT INTO "license_employee" ("license", "employee")
+	      VALUES ($1, $2)
+	      ON CONFLICT DO NOTHING`
+	if _, execErr := scope.conn.ExecContext(scope.ctx, q, license.ID, data.Employee); execErr != nil {
+		return errors.Wrapf(execErr,
+			"user %q of account %q with token %q tried to add employee %q to the license %q",
+			token.UserID, token.User.AccountID, token.ID, data.Employee, license.ID)
+	}
+	return nil
+}
+
+// DeleteEmployee TODO issue#docs
+func (scope licenseManager) DeleteEmployee(token *types.Token, data query.LicenseEmployee) error {
+	license, readErr := scope.Read(token, query.ReadLicense{ID: data.ID})
+	if readErr != nil {
+		return readErr
+	}
+	q := `DELETE FROM "license_employee"
+	       WHERE "license" = $1 AND "employee" = $2`
+	if _, execErr := scope.conn.ExecContext(scope.ctx, q, license.ID, data.Employee); execErr != nil {
+		return errors.Wrapf(execErr,
+			"user %q of account %q with token %q tried to delete employee %q from the license %q",
+			token.UserID, token.User.AccountID, token.ID, data.Employee, license.ID)
+	}
+	return nil
+}
+
+// AddWorkplace TODO issue#docs
+func (scope licenseManager) AddWorkplace(token *types.Token, data query.LicenseWorkplace) error {
+	license, readErr := scope.Read(token, query.ReadLicense{ID: data.ID})
+	if readErr != nil {
+		return readErr
+	}
+	q := `INSERT INTO "license_workplace" ("license", "workplace")
+	      VALUES ($1, $2)
+	      ON CONFLICT DO NOTHING`
+	if _, execErr := scope.conn.ExecContext(scope.ctx, q, license.ID, data.Workplace); execErr != nil {
+		return errors.Wrapf(execErr,
+			"user %q of account %q with token %q tried to add workplace %q to the license %q",
+			token.UserID, token.User.AccountID, token.ID, data.Workplace, license.ID)
+	}
+	return nil
+}
+
+// DeleteWorkplace TODO issue#docs
+func (scope licenseManager) DeleteWorkplace(token *types.Token, data query.LicenseWorkplace) error {
+	license, readErr := scope.Read(token, query.ReadLicense{ID: data.ID})
+	if readErr != nil {
+		return readErr
+	}
+	q := `DELETE FROM "license_workplace"
+	       WHERE "license" = $1 AND "workplace" = $2`
+	if _, execErr := scope.conn.ExecContext(scope.ctx, q, license.ID, data.Workplace); execErr != nil {
+		return errors.Wrapf(execErr,
+			"user %q of account %q with token %q tried to delete workplace %q from the license %q",
+			token.UserID, token.User.AccountID, token.ID, data.Workplace, license.ID)
+	}
+	return nil
+}
+
+// PushWorkplace TODO issue#docs
+func (scope licenseManager) PushWorkplace(token *types.Token, data query.LicenseWorkplace) error {
+	license, readErr := scope.Read(token, query.ReadLicense{ID: data.ID})
+	if readErr != nil {
+		return readErr
+	}
+	q := `UPDATE "license_workplace"
+	         SET "updated_at" = now()
+	       WHERE "license" = $1 AND "workplace" = $2`
+	if _, execErr := scope.conn.ExecContext(scope.ctx, q, license.ID, data.Workplace); execErr != nil {
+		return errors.Wrapf(execErr,
+			"user %q of account %q with token %q tried to push workplace %q of the license %q",
+			token.UserID, token.User.AccountID, token.ID, data.Workplace, license.ID)
+	}
+	return nil
+}
+
+// issue#draft }
