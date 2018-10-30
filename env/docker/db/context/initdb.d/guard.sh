c595061002d77537ad51efb59575c1d14a8c64f2
Kamil Samigullin <kamil@samigullin.info>
2018-10-30 21:50:02 +0300

    issue #64: do not build custom db

--- env/docker/db/context/initdb.d/guard.sh
+++ env/docker/db/context/initdb.d/guard.sh
@@ -2,6 +2,8 @@
 
 set -e
 
-psql -v ON_ERROR_STOP=1 --username "${POSTGRES_USER}" <<-EOSQL
-    \c "${POSTGRES_DB}";
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+    CREATE USER "guard" WITH PASSWORD 'guard';
+    CREATE DATABASE "guard";
+    GRANT ALL PRIVILEGES ON DATABASE "guard" TO "guard";
 EOSQL
